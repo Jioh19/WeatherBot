215383b7b67e53d12cb2e9d9d0ef2f760776deea
jioh19 <jioh1982@gmail.com>
2025-03-20 11:30:14 -0300

    Fix bug in BotReader

--- Reader/BotReader.cs
+++ Reader/BotReader.cs
@@ -11,8 +11,33 @@ public class BotReader :IReader<Dictionary<string, Bot>>
 
         try
         {
-        Dictionary<string, Bot> bots = JsonConvert.DeserializeObject<Dictionary<string, Bot>>(jsonString);
+        Dictionary<string, dynamic> unclassifiedBots = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(jsonString);
+        
+        Console.WriteLine(jsonString);
 
+        Dictionary<string, Bot> bots = new Dictionary<string, Bot>();
+
+        foreach (var kpv in unclassifiedBots)
+        {
+            var data = kpv.Value;
+            Bot bot = new Bot();
+            bot.Enabled = data.enabled;
+            bot.Message = data.message;
+            
+            if (data.humidityThreshold != null)
+            {
+                bot.Type = Bot.BotType.HUMIDITY;
+                bot.Value = (int)data.humidityThreshold;
+            }
+            else if (data.temperatureThreshold != null)
+            {
+                bot.Type = Bot.BotType.TEMPERATURE;
+                bot.Value = (int)data.temperatureThreshold;
+            }
+
+            bots.Add(kpv.Key, bot);
+        }
+        
         return bots;
             
         } 
